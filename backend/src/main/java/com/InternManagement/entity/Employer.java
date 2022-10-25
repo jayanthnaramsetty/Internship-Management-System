6e2123920339d076bb53658d3748c6abe1237496
Likhitha Gopi <89407077+Likhitha-Gopi@users.noreply.github.com>
2022-10-24 22:28:55 -0500

    Added Employer details java  file to the repo
    Added Employer details java  file to the repo

---
+++ backend/src/main/java/com/InternManagement/entity/Employer.java
@@ -0,0 +1,98 @@
+package com.InternManagement.entity;
+
+public class Employer {
+	
+	 private String companyName;
+	 private String emailId;
+	 private String phoneNumber;
+	 private String streetAddress;
+	 private String city;
+	 private String state;
+	 private String zipcode;
+	 private String startDate;
+	 private int applicationNumber;
+	 public int getApplicationNumber() {
+		return applicationNumber;
+	}
+	public void setApplicationNumber(int applicationNumber) {
+		this.applicationNumber = applicationNumber;
+	}
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(String zipcode) {
+		this.zipcode = zipcode;
+	}
+	public String getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(String startDate) {
+		this.startDate = startDate;
+	}
+	public String getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(String endDate) {
+		this.endDate = endDate;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getStipend() {
+		return stipend;
+	}
+	public void setStipend(String stipend) {
+		this.stipend = stipend;
+	}
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	private String endDate;
+	 private String role;
+	 private String stipend;
+	 private String studentId;
+	
+	
+}
