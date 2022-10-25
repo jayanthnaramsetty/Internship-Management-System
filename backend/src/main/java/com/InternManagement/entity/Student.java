4c5047f3d4ee84ee51c0f9412bafabc18f3b7cfb
Jayanth Naramsetty <89616595+jayanthnaramsetty@users.noreply.github.com>
2022-10-24 21:57:12 -0500

    Add files via upload
    Added Student details java file in the application

---
+++ backend/src/main/java/com/InternManagement/entity/Student.java
@@ -0,0 +1,61 @@
+package com.InternManagement.entity;
+
+import java.sql.Timestamp;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class Student {
+	
+	private String username;
+	private String firstName;
+	private String lastName;
+	private String gender;
+	private String studentId;
+	private String emailId;
+	private String phoneNumber;
+
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	
+}
