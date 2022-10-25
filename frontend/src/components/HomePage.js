58cf3782680afbab75f84b67044b3fc86a147998
Sai Sharma <89261592+saisharma9@users.noreply.github.com>
2022-10-24 23:04:45 -0500

    fetching user id and role

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -1,3 +1,23 @@
 import React from "react";
 import { withRouter } from "react-router";
 import axios from "axios";
+
+class HomePage extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        userId:  localStorage.getItem("userId"),
+        userRole: localStorage.getItem("userRole"),
+        projectDatas : []
+      };
+    }
+  
+    handleLogout = () => {
+      localStorage.removeItem("userId");
+      localStorage.removeItem("userRole");
+      const {
+        history: { push },
+      } = this.props;
+      push("/");
+    };
+}  
