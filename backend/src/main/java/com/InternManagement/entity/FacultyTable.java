5b987c31cca0f4f5522ea5c75ce6d9c49a96d373
Pooja Vankineni <89403383+poojaVankineni@users.noreply.github.com>
2022-10-24 22:04:03 -0500

    Add files via upload
    Added Faculty details

---
+++ backend/src/main/java/com/InternManagement/entity/FacultyTable.java
@@ -0,0 +1,82 @@
+package com.InternManagement.entity;
+
+import java.sql.Timestamp;
+
+public class FacultyTable {
+	
+	private String id;
+	 private int applicationNumber;
+	 private String status;
+	 private String studentId;
+	 private String department;
+	 private String firstName;
+	 private String lastName;
+	 private String emailId;
+	 private String state;
+	 private String semester;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getApplicationNumber() {
+		return applicationNumber;
+	}
+	public void setApplicationNumber(int applicationNumber) {
+		this.applicationNumber = applicationNumber;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getStudentId() {
+		return studentId;
+	}
+	public void setStudentId(String studentId) {
+		this.studentId = studentId;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getSemester() {
+		return semester;
+	}
+	public void setSemester(String semester) {
+		this.semester = semester;
+	}
+	
+	
+	 
+	
+	
+}
