b8e46c0fd0510e800e213d516fcc6fb4b7103fd6
Sai Sharma <89261592+saisharma9@users.noreply.github.com>
2022-10-24 23:12:22 -0500

    Update HomePage.js

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -38,5 +38,36 @@ class HomePage extends React.Component {
         push('/viewApplication');
     
       }
+
+      handleHome = () =>{
+        const {userId} = this.state;
+        const {
+          history: { push },
+        } = this.props;
+        push('/home');
+    
+      }
+    
+      handleLogout= () =>{
+        const {userId} = this.state;
+        const {
+          history: { push },
+        } = this.props;
+        push('/');
+    
+      }
+    
+      render() {
+        const {userId,userRole,projectDatas} = this.state;
+        return (
+    
+    <div className=" flex flex-col" style={{backgroundColor:'aliceBlueBlue',minHeight:'100vh'}}>
+      {/* <div style={{backgroundColor:'white',display:'flex',padding:'12px',borderBottom:'1px solid white',fontSize:'24px'}}> */}
+        <div style={{backgroundColor:'white',display:'flex',padding:'12px',color:'#006747'}}>
+            <div style={{display:'flex',flexGrow:'1'}}>
+            <img alt="Northwest Missouri State University" height="79px" src="https://www.nwmissouri.edu/layout/v2019/images/svg/logo-n.svg" width="74px"></img>
+            <div style={{fontSize:'24px',fontWeight:'600',width:'100%',display:'flex',padding:'20px',alignItems:'center',color:'#006747'}}>NORTHWEST MISSOURI STATE UNIVERSITY</div>
+          </div>
+    
     
 }  
